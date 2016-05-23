578f5aa2351023623764ab1ab8d76c3f19dac271
papiroca-tm <danshin.workspace@gmail.com>
2016-05-23 23:30:26 +0300

    вывод в rabbitServer в std и file (2 типа text и  json)

--- rabbitServer/main.go
+++ rabbitServer/main.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 	//"strconv"
 	//"strings"
-	//"time"
 	"github.com/streadway/amqp"
 )
 
@@ -108,6 +108,9 @@ func main() {
 
 }
 
+/*
+receiveMessage ...
+*/
 func receiveMessage(msgByteArr []byte) {
 	// сначала распарсим в структуру
 	var message Msg
@@ -128,14 +131,101 @@ func receiveMessage(msgByteArr []byte) {
 sendMsgToStdout ...
 */
 func sendMsgToStdout(message Msg) {
-	//msgType := message.MsgType
-	// ... todo
+	msgType := message.MsgType
+	formatStr := "%s %s :%s:%s:%s:%s:%s:%s: %s\n"
+	switch msgType {
+	case "TRACE":
+		if settings.StdOutTrace == true {
+			stdPrint(formatStr, message)
+		}
+	case "INFO":
+		formatStr = "%s  %s :%s:%s:%s:%s:%s:%s: %s\n"
+		if settings.StdOutInfo == true {
+			stdPrint(formatStr, message)
+		}
+	case "WARN":
+		formatStr = "%s  %s :%s:%s:%s:%s:%s:%s: %s\n"
+		if settings.StdOutWarn == true {
+			stdPrint(formatStr, message)
+		}
+	case "ERROR":
+		if settings.StdOutError == true {
+			stdPrint(formatStr, message)
+		}
+	}
+}
+
+/*
+stdPrint ...
+*/
+func stdPrint(fmtStr string, msg Msg) {
+	fmt.Printf(fmtStr, msg.MsgType, msg.DtTimeStr, msg.ErrCode, msg.AppName, msg.PkgName, msg.ModuleName, msg.FuncName, msg.Line, msg.LogText)
 }
 
 /*
 writeMsgToFile ...
 */
 func writeMsgToFile(message Msg, method string) {
-	//msgType := message.MsgType
-	// ... todo
+	t := time.Now()
+	filePath := settings.LogFilePath + "/" + settings.FileOutMethod + "/" + t.Format("02-01-2006") + "/"
+	fileName := settings.LogFileName
+	s := settings.LogFileSeparator
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		err = os.MkdirAll(filePath, 0666)
+		failOnError(err, "ошибка создания директории")
+	}
+	if _, err := os.Stat(filePath + fileName); os.IsNotExist(err) {
+		_, err := os.Create(filePath + fileName)
+		failOnError(err, "ошибка создания файла")
+	}
+
+	if method == "text" {
+		var formatStr string
+		msgType := message.MsgType
+		switch msgType {
+		case "INFO", "WARN":
+			formatStr = "%s " + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s\n"
+		default:
+			formatStr = "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s" + s + "%s\n"
+		}
+		str := fmt.Sprintf(
+			formatStr,
+			message.MsgType,
+			message.DtTimeStr,
+			message.ErrCode,
+			message.AppName,
+			message.PkgName,
+			message.ModuleName,
+			message.FuncName,
+			message.Line,
+			message.LogText,
+			message.LogContext,
+		)
+		writeStrToFile(filePath+fileName, str)
+	}
+	if method == "json" {
+		str, err := msgToJSON(message)
+		failOnError(err, "ошибка msgToJSON(message)")
+		writeStrToFile(filePath+fileName, str+"\n")
+	}
+}
+
+/*
+writeStrToFile ...
+*/
+func writeStrToFile(file string, str string) {
+	f, err := os.OpenFile(file, os.O_APPEND, 0666)
+	failOnError(err, "ошибка открытия файла для записи")
+	n, err := f.WriteString(str)
+	failOnError(err, "ошибка записи строки в файл"+string(n))
+	defer f.Close()
+}
+
+/*
+msgToJSON ...
+*/
+func msgToJSON(m Msg) (string, error) {
+	jsonMsg, err := json.Marshal(m)
+	failOnError(err, "ошибка парсинга сообщения в JSON")
+	return string(jsonMsg), nil
 }
